2ef98dd5411d9b41cc33b968f13b00aca6e9a1e2
rocha-bruno <rocha.bruno.alexandre@gmail.com>
2020-10-25 18:05:00 +0100

    Add import linting

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ def tests(session):
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
